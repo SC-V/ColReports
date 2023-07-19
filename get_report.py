5c44b8fb37361c3552b1f7e78fd2dc26ce0bdfb6
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-07-19 13:15:09 -0500

    Update get_report.py

--- get_report.py
+++ get_report.py
@@ -247,6 +247,7 @@ def get_report(client_option="All clients", option="Today", start_=None, end_=No
     if client_option == "All clients":
       client_number = 0  
       for secret in CLAIM_SECRETS:
+          st.write(client_number)
           claims, cursor = get_claims(secret, date_from, date_to)
           while cursor:
               new_page_claims, cursor = get_claims(secret, date_from, date_to, cursor)
